aebdadb27e829419f72bbad16a6f36052f89c943
Bharanishr <113794403+Bharanishr@users.noreply.github.com>
2022-09-22 10:58:43 +0530

    Add files via upload

---
+++ Assignment/Team Member 2 - Bharanishree V/Assignement 1/String Operations.py
@@ -0,0 +1,16 @@
+txt1="hello world"
+
+print(txt1)
+
+#reverse a string
+print("Reversed String: " + txt1[::-1]) 
+
+#slicing
+print("Sliced String: " + txt1[slice(0,3,2)]) 
+
+txt2=" Good Morning "
+
+#concatenation
+str=txt1+txt2 
+
+print("Concatenated String: " + str)
